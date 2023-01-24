b7a4a9b3b36ef723975d11e1aa4cb2e479e0b11c
dlwnstn0524 <ljs052450@gmail.com>
2023-01-25 00:51:14 +0900

    [Add] layout_exeercise add

---
+++ name_picker_2/android/app/src/main/kotlin/com/example/name_picker_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.name_picker_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ name_picker_2/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Name Picker ^_^'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class Suggestion extends StatefulWidget {
+  const Suggestion({super.key});
+
+  @override
+  State<Suggestion> createState() => _SuggestionState();
+}
+
+class _SuggestionState extends State<Suggestion> {
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle messageStyle = TextStyle(fontSize: 20, color: Colors.white);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            Text('My recommand is ...!'),
+            Text("word!!"),
+            TextButton(onPressed: () {}, child: Text("Press")),
+          ],
+        ));
+  }
+}
