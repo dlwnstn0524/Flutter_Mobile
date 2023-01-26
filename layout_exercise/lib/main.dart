372859529f6e4caf22251e24417b63629b6e225b
dlwnstn0524 <ljs052450@gmail.com>
2023-01-26 21:17:04 +0900

    [Finish] Layout_Coulmn Practice Finish

--- layout_exercise/lib/main.dart
+++ layout_exercise/lib/main.dart
@@ -15,6 +15,16 @@ class ColumnExample extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          /*
+          Column의 mainAxisAlignment 요소 정리
+          1. MainAxisAlignment.start : 상단부터 차곡차곡 쌓인다.
+          2. MainAxisAlignment.center : 가운데로 정렬한다.
+          3. MainAxisAlignment.end : 하단부터 차곡차곡 쌓인다.
+          4. MainAxisAlignment.spaceAround : 양끝과 위젯간에 간격이 생긴다. 위젯 간격 절반만큼 양끝 여백 
+          5. MainAxisAlignment.spaceBetween : 여백없이 정렬이 된다.
+          6. MainAxisAlignment.spaceEvenly : 양끝과 위젯간에 간격이 생긴다. 모든 간격 동일
+          */
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
               width: 100,
